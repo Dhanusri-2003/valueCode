a2e711a0d2ad84eb153b86c9c756cc18d0ae9419
Dhanusri-2003 <skdhanusri123@gmail.com>
2025-11-14 11:15:54 +0530

    update url

--- app/api/send_n8n/route.js
+++ app/api/send_n8n/route.js
@@ -2,7 +2,7 @@ export async function POST(req) {
   const body = await req.json();
 
   const response = await fetch(
-    "https://n8n.srv1105022.hstgr.cloud/webhook-test/aab213c1-8a38-4e3a-8050-d03df672da73",
+    "https://n8n.srv1105022.hstgr.cloud/webhook/aab213c1-8a38-4e3a-8050-d03df672da73",
     {
       method: "POST",
       headers: { "Content-Type": "application/json" },
